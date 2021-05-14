d8f99e90ebb75bc3a0375bcd28ba03e2bf316b75
feng <123@qq.com>
2021-05-15 00:38:35 +0800

    udpate

--- quotation/quotation.go
+++ quotation/quotation.go
@@ -15,25 +15,70 @@ func Main(setting structs.Setting)  {
 
 //获取历史100数据
 func history(setting structs.Setting) {
-	data := utils.Get("/api/v5/market/history-candles?instId=" + setting.InstId)
+	// data := utils.Get("/api/v5/market/history-candles?instId=" + setting.InstId)
 
+	data := [11][2]string{
+		{"1","1"},
+		{"2","2"},
+		{"3","3"},
+		{"4","4"},
+		{"5","5"},
+		{"6","6"},
+		{"7","7"},
+		{"8","8"},
+		{"9","9"},
+		{"10","10"},
+		{"11","11"},
+	}
 	var ma5 []float64
 	var ma10 []float64
-
-	for i := len(data)-1; i>0; i-- {
+	var t5 int
+	var t10 int
+	var tempCount float64
+	var tempVal float64
+	for i := len(data)-1; i>=0; i-- {
 		switch reflect.TypeOf(data[i]).Kind() {
 			case reflect.Slice, reflect.Array:
 				s := reflect.ValueOf(data[i])
-				// for i := 0; i < s.Len(); i++ {
-				// 	fmt.Println(s.Index(i))
-				// }
-
-				fmt.Println(s.Index(0))
-
 				cur,_ := strconv.ParseFloat(s.Index(1).Interface().(string), 32)
 
-				ma5 = append(ma5, cur)
-				ma10 = append(ma10, cur)
+				if i == len(data)-1 {
+					ma5 = append(ma5, cur)
+					ma10 = append(ma10, cur)
+				}else{
+					var ma5Temp float64 = 0
+					var ma10Temp float64 = 0
+					tempCount = 0
+					t5 = 4
+					t10 = 9
+					for j := i+1; j < len(data); j++ {
+						if t10 <= 0 {
+							break
+						}
+
+						tempVal, _ = strconv.ParseFloat(reflect.ValueOf(data[j]).Index(1).Interface().(string), 32)
+						tempCount = tempCount + tempVal
+
+						ma10Temp = tempCount
+
+						if t5 == 1 {
+							ma5Temp = tempCount
+						}
+
+						t5 --
+						t10 --
+					}
+
+					if ma5Temp == 0 {
+						ma5Temp = ma10Temp
+					}
+
+					ma5Temp = ma5Temp + cur
+					ma10Temp = ma10Temp + cur
+
+					ma5 = append(ma5, ma5Temp)
+					ma10 = append(ma10, ma10Temp)
+				}
 
 			case reflect.String:
 				s := reflect.ValueOf(data[i])
@@ -46,6 +91,7 @@ func history(setting structs.Setting) {
 	}
 
 	fmt.Println(ma5)
+	fmt.Println(ma10)
 }
 
 //获取当前数据
