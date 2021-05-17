e352fffaca0b35c86705c74f8e6f547d300e4df7
feng <123@qq.com>
2021-05-17 17:55:16 +0800

    update

--- trader/trader.go
+++ trader/trader.go
@@ -6,6 +6,10 @@ import (
 	"../utils"
 )
 
+func Td1(setting structs.Setting){
+	order(setting.InstId, "cross", "buy", "market", "1","long")
+}
+
 //tdMode	String	是	交易模式
 						// 保证金模式：isolated：逐仓 ；cross：全仓
 						// 非保证金模式：cash：非保证金
@@ -17,12 +21,15 @@ import (
 						// fok：全部成交或立即取消
 						// ioc：立即成交并取消剩余
 // sz	String	是	委托数量
-func Td1(setting structs.Setting){
-	order(setting.InstId, "cross", "buy", "market", "1")
-}
-
-func order(instId string, tdMode string, side string, ordType string, sz string){
-	data := map[string]string {"instId":instId,"tdMode":tdMode,"side":side,"ordType":ordType,"sz":sz}
+// posSide
+	// 持仓方向，单向持仓模式下此参数非必填，如果填写仅可以选择net；在双向持仓模式下必填，且仅可选择 long 或 short。
+	// 双向持仓模式下，side和posSide需要进行组合
+	// 开多：买入开多（side 填写 buy； posSide 填写 long ）
+	// 开空：卖出开空（side 填写 sell； posSide 填写 short ）
+	// 平多：卖出平多（side 填写 sell；posSide 填写 long ）
+	// 平空：买入平空（side 填写 buy； posSide 填写 short ）
+func order(instId string, tdMode string, side string, ordType string, sz string, posSide string){
+	data := map[string]string {"instId":instId,"tdMode":tdMode,"side":side,"ordType":ordType,"sz":sz, "posSide":posSide}
 	res := utils.Post("/api/v5/trade/order", data)
 	fmt.Println(res)
 }
